38fe9a0ceadbd1cd51adf6f59eb8894346334739
solo samurai <ahmed_abdikani@hotmail.com>
2025-01-13 18:15:56 +0300

    add new logo components and update existing logos for consistency

---
+++ src/components/logos/UnifyLogoV2.vue
@@ -0,0 +1,12 @@
+<!-- UnifyLogo.vue -->
+<template>
+  <img src="../../assets/images/logo-mini.png" alt="Unify Logo" v-if="mini" />
+
+  <img src="../../assets/images/logo.png" alt="Unify Logo" v-else />
+</template>
+
+<script setup lang="ts">
+const { mini } = defineProps<{
+  mini?: boolean;
+}>();
+</script>

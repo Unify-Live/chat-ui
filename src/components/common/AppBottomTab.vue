0ff27deff260d859d511fa68d3ab404cb3767d13
solo samurai <ahmed_abdikani@hotmail.com>
2025-01-13 18:18:21 +0300

    add AppBottomTab and AppHeader components for improved navigation and notifications

---
+++ src/components/common/AppBottomTab.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav
+    class="flex items-center gap-7 justify-around p-3 overflow-y-auto md:hidden"
+  >
+    <router-link
+      :to="item.route"
+      v-slot="{ isActive }"
+      v-for="item in navItems"
+      :key="item.label"
+    >
+      <button
+        class="flex flex-col items-center gap-0.5"
+        :class="{ 'text-primary': isActive }"
+      >
+        <component
+          :is="item.icon"
+          :class="{ '*:fill-primary': isActive }"
+          class="size-5"
+        />
+        {{ item.label }}
+      </button>
+    </router-link>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import OpenMoreOptionsLogo from "@/components/logos/OpenMoreOptionsLogo.vue";
+import ChatsLogo from "@/components/logos/ChatsLogo.vue";
+import HomeLogo from "@/components/logos/HomeLogo.vue";
+import ProjectLogo from "../logos/ProjectLogo.vue";
+
+const navItems = [
+  { route: "/projects", icon: HomeLogo, label: "Головна" },
+
+  { route: `/chat`, icon: ChatsLogo, label: "Чат" },
+
+  { route: "/project-details", icon: ProjectLogo, label: "Проєкти" },
+  {
+    route: "/more",
+    icon: OpenMoreOptionsLogo,
+    label: "Меню",
+  },
+];
+</script>
---
+++ src/components/common/AppHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex items-center justify-between px-4 py-3 md:hidden">
+    <UnifyLogo />
+
+    <div class="flex gap-6 items-center">
+      <button @click="getNotifications">
+        <n-badge :value="2" :offset="[-2, -2]">
+          <n-icon size="20">
+            <NotificationsLogo />
+          </n-icon>
+        </n-badge>
+      </button>
+
+      <ThemeChanger />
+
+      <router-link to="/profile" custom v-slot="{ href, navigate, isActive }">
+        <button
+          :href="href"
+          @click="navigate"
+          :class="isActive ? 'text-primary' : 'text-gray-600'"
+        >
+          <img
+            class="bg-black h-8 rounded-full aspect-square"
+            src="https://i.pravatar.cc/300"
+            preview-disabled
+          />
+        </button>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import UnifyLogo from "@/components/logos/UnifyLogoV2.vue";
+
+const getNotifications = () => {
+  console.log("Getting notifications");
+};
+</script>

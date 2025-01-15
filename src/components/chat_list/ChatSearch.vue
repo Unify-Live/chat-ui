2fdaa10e02cbd7a1710ab475f63fbcca454bf33d
solo samurai <ahmed_abdikani@hotmail.com>
2025-01-15 23:14:36 +0300

    fix: ChatSearch component layout for improved structure

--- src/components/chat_list/ChatSearch.vue
+++ src/components/chat_list/ChatSearch.vue
@@ -1,35 +1,37 @@
 <template>
-  <!-- message search -->
-
-  <div class="m-4 flex items-center gap-2 rounded-lg border p-2">
-    <n-icon :component="Search" size="18" color="#E5E7EB" />
-
-    <input
-      v-model="search_text"
-      type="text"
-      placeholder="Type a message..."
-      class="flex-1 outline-gray-300"
-    />
-  </div>
-
-  <!-- message filters -->
-
   <div>
-    <n-scrollbar x-scrollable>
-      <div class="flex max-w-72 gap-4 p-4">
-        <button
-          v-for="filter in filters"
-          :key="filter.name"
-          @click="selected_filter = filter"
-          class="w-fit whitespace-nowrap rounded-full bg-gray-100 px-3 py-1.5 text-sm"
-          :class="{
-            '!bg-primary text-white': selected_filter === filter,
-          }"
-        >
-          {{ filter.name }} ({{ filter.l }})
-        </button>
-      </div>
-    </n-scrollbar>
+    <!-- message search -->
+
+    <div class="m-4 flex items-center gap-2 rounded-lg border p-2">
+      <n-icon :component="Search" size="18" color="#E5E7EB" />
+
+      <input
+        v-model="search_text"
+        type="text"
+        placeholder="Type a message..."
+        class="flex-1 outline-gray-300"
+      />
+    </div>
+
+    <!-- message filters -->
+
+    <div>
+      <n-scrollbar x-scrollable>
+        <div class="flex max-w-72 gap-4 p-4">
+          <button
+            v-for="filter in filters"
+            :key="filter.name"
+            @click="selected_filter = filter"
+            class="w-fit whitespace-nowrap rounded-full bg-gray-100 px-3 py-1.5 text-sm"
+            :class="{
+              '!bg-primary text-white': selected_filter === filter,
+            }"
+          >
+            {{ filter.name }} ({{ filter.l }})
+          </button>
+        </div>
+      </n-scrollbar>
+    </div>
   </div>
 </template>
 

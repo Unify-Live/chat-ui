826a5c27d735e1e7273f640c845126cda7b27444
solo samurai <ahmed_abdikani@hotmail.com>
2025-01-15 15:03:41 +0300

    add BackButton component for improved navigation and set baseUrl in tsconfig

---
+++ src/components/common/BackButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button
+    @click="path ? router.replace(path) : router.back()"
+    class="md:hidden"
+  >
+    <n-icon :component="ChevronBack" size="18" />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { ChevronBack } from "@vicons/ionicons5";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const { path } = defineProps<{
+  path?: string;
+}>();
+</script>

2d8aefc09327257e01c4155fed5d714e6b2fa962
solo samurai <ahmed_abdikani@hotmail.com>
2025-01-16 00:11:58 +0300

    fix: scroll to last message

--- src/components/chat/ChatWindow.vue
+++ src/components/chat/ChatWindow.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex flex-1 flex-col">
+  <div class="odd: flex h-screen flex-1 flex-col overflow-hidden">
     <ChatHeader />
-    <div class="flex-1 bg-light-gray">
+    <div class="flex-1 overflow-y-hidden bg-light-gray">
       <MessageList
         v-if="chatStore.chatOpened"
         :messages="chatStore.messagesList"
@@ -12,16 +12,13 @@
   </div>
 </template>
 
-<script setup>
-import { ref, onMounted } from "vue";
+<script setup lang="ts">
+import { onMounted } from "vue";
 import ChatInput from "./ChatInput.vue";
-import ChatMessage from "./ChatMessage.vue";
 import MessageList from "./MessageList.vue";
 import { useChatStore } from "@/stores/chat";
 import ChatHeader from "./ChatHeader.vue";
 
-const newMessage = ref("");
-const messagesRef = ref(null);
 const chatStore = useChatStore();
 
 // Props
--- src/components/chat/MessageList.vue
+++ src/components/chat/MessageList.vue
@@ -1,38 +1,41 @@
 <template>
-  <div ref="swipeRef">
-    <
-    <n-virtual-list
-      v-if="chatStore.messagesList.length > 0"
-      :items="chatStore.messagesList"
-      :item-size="10"
-      item-resizable
-      :key-field="'uuid'"
-      style="max-height: 500px"
-    >
-      <template #default="{ item: message }">
-        <div class="mb-4">
-          <ChatMessage :message="message" />
-        </div>
-      </template>
-    </n-virtual-list>
-    <div
-      v-else
-      @click="
-        () => {
-          $router.push('/chat/' + chatStore.selectedChat?.uuid + '/details');
-        }
-      "
-    >
-      No messages yet.
-    </div>
-  </div>
+  <n-virtual-list
+    v-if="chatStore.messagesList.length > 0"
+    :items="chatStore.messagesList"
+    :item-size="10"
+    item-resizable
+    :key-field="'uuid'"
+    ref="virtualListInst"
+  >
+    <template #default="{ item: message }">
+      <div class="mb-4">
+        <ChatMessage :message="message" />
+      </div>
+    </template>
+  </n-virtual-list>
+  <div v-else>No messages yet.</div>
 </template>
 
 <script setup lang="ts">
 import { useChatStore } from "@/stores/chat";
 import ChatMessage from "./ChatMessage.vue";
+import { VirtualListInst } from "naive-ui";
+import { ref, watchEffect } from "vue";
 
+const virtualListInst = ref<VirtualListInst>();
 const chatStore = useChatStore();
+
+function handleScrollToPosition() {
+  virtualListInst.value?.scrollTo({
+    index: chatStore.messagesList.length - 1,
+    debounce: true,
+  });
+}
+
+watchEffect(() => {
+  chatStore;
+  handleScrollToPosition();
+});
 </script>
 
 <style scoped></style>
